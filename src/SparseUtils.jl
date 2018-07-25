bdf9b6584fe30a856099b7cf0d3bcf934115d1cd
John Lapeyre <lapeyre.math122a@gmail.com>
2018-07-25 22:30:51 +0200

    finish all prune and drop functions
    These functions act on CSC sparse matricse.

--- src/SparseUtils.jl
+++ src/SparseUtils.jl
@@ -2,7 +2,8 @@ module SparseUtils
 
 export c_to_julia_index, c_to_julia_index!, sparsity,
     nnzcounts, sparse_stats, nrows, ncols,
-    prunecols!, dropzerocols, dropzerorows
+    prunecols!, dropzerocols, dropzerorows, dropzerorowscols,
+    hasemptycols, hasemptyrows
 
 import SparseArrays
 import SparseArrays: SparseMatrixCSC, nnz
@@ -104,24 +105,91 @@ in each column of `sp`.
 """
 nnzcols(sp::SparseMatrixCSC) = (nnz(sp, i) for i in 1:ncols(sp))
 
+"""
+    prunecols!(sp::SparseMatrixCSC, min_connections)
+
+For each column, set all elements of the column equal to zero if the number of non-zero
+elements is less than `min_connections`. `sp` is modified in place. Here "zero" means
+structural zeros.
+"""
 function prunecols!(sp::SparseMatrixCSC, min_connections)
     connection_flags = [nnz(sp, j) >= min_connections for j in 1:ncols(sp)] # This must not change during fkeep!
     SparseArrays.fkeep!(sp, (i, j, v) -> connection_flags[j], true)
     return sp
 end
 
+"""
+    hasemptycols(sp::SparseMatrixCSC)
+
+Return true if `sp` contains columns for which all elements are structural
+zeros.
+
+`prunecols!` will create such columns.
+"""
+hasemptycols(sp::SparseMatrixCSC) = length(sp.colptr) != length(unique(sp.colptr))
+
+"""
+    dropzerocols(sp::SparseMatrixCSC)
+
+If `sp` has "empty" columns as determined by `hasemptycols`, then remove these
+columns and renumber the remaining columns. A new `SparseMatrixCSC` is returned.
+Modifying elements of `.rowval` and `.nzval` in the output matrix will change
+these values in `sp`.
+"""
 function dropzerocols(sp::SparseMatrixCSC)
-    newcolptr = unique(sp.colptr)
-    n = length(newcolptr) - 1
+    (newcolptr, n) = _dropzerocols(sp)
     SparseMatrixCSC(sp.m, n, newcolptr, sp.rowval, sp.nzval)
 end
 
+function _dropzerocols(sp::SparseMatrixCSC)
+    newcolptr = unique(sp.colptr)
+    n = length(newcolptr) - 1
+    return (newcolptr, n)
+end
+
+"""
+    hasemptyrows(sp::SparseMatrixCSC)
+
+Return true if `sp` contains rows for which all elements are structural
+zeros.
+
+`prunecols!` may create such rows.
+"""
+function hasemptyrows(sp::SparseMatrixCSC)
+    rowlist = sort!(unique(sp.rowval))
+    return length(rowlist) != last(rowlist)
+end
+
+"""
+    dropzerorow(sp::SparseMatrixCSC)
+
+If `sp` has "empty" rows as determined by `hasemptycols`, then remove these
+columns and renumber the remaining columns. A new `SparseMatrixCSC` is returned.
+Modifying elements of `.rowval` and `.nzval` in the output matrix will change
+these values in `sp`.
+"""
 function dropzerorows(sp::SparseMatrixCSC)
+    (renumbered_rowval, m) = _dropzerorows(sp)
+    SparseMatrixCSC(m, sp.n, sp.colptr, renumbered_rowval, sp.nzval)
+end
+
+function _dropzerorows(sp::SparseMatrixCSC)
     sorted_rowval = sort!(unique(sp.rowval))
     rowval_type = eltype(sp.rowval)
     renumbered_rowval = rowval_type[searchsortedfirst(sorted_rowval, val) for val in sp.rowval]
     m = length(sorted_rowval)
-    SparseMatrixCSC(m, sp.n, sp.colptr, renumbered_rowval, sp.nzval)
+    return (renumbered_rowval, m)
+end
+
+"""
+    dropzerorowscols(sp::SparseMatrixCSC)
+
+Perform both `dropzerorows` and `dropzerocols` on `sp`
+"""
+function dropzerorowscols(sp::SparseMatrixCSC)
+    (newcolptr, n) = _dropzerocols(sp)
+    (renumbered_rowval, m) = _dropzerorows(sp)
+    return SparseMatrixCSC(m, n, newcolptr, renumbered_rowval, sp.nzval)
 end
 
 """
